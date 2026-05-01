20987ba2896779625f5bd244f7d73e5f977bec85
jariman <javierharo97@gmail.com>
2026-05-01 17:58:56 +0200

    Primer dia

---
+++ ejercicio_array.js
@@ -0,0 +1,16 @@
+
+const names = ["Mario","Lucia","Elena","Pablo"]
+
+console.log(names[0]) //Primer nombre
+
+console.log(names[names.length - 1]) //Ultimo nombre
+
+console.log(names.length) //Cuantos nombre hay
+
+names.push["Sofia"] //Anadir Sofia a la array
+
+names.pop //Quitar ultimo nombre
+
+names[1] = "Carlos"
+
+console.log(names)
---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Primera
+
+function present (name1,name2,age){
+
+    return "Hola " + name1 + " " + name2 + ", tu edad es " + age +", " +  "bienvenido!!";
+
+}
+
+console.log (present("Javier", "Martinez", 28));
+console.log (present("Nuria", "Mane", 25));
